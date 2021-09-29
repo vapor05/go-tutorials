ae7d0785c037be4b173df618ab94c2a87b934d1c
vapor05 <nicholasbocchini@gmail.com>
2021-09-28 20:52:18 -0500

    Finished accessing a relational database tutorial

--- data-access/main.go
+++ data-access/main.go
@@ -38,6 +38,25 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	// Hard-code ID 2 here to test the query
+	alb, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
+
+	// add an album
+	albID, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("ID of added album: %v\n", albID)
 }
 
 // queries for albums that have the specified artist name
@@ -66,4 +85,36 @@ func albumsByArtist(name string) ([]Album, error) {
 	}
 
 	return albums, nil
+}
+
+// albumByID queries for the album with the specified ID
+func albumByID(id int64) (Album, error) {
+	// An album to hold data from the returned row
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = $1", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("ablumById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("ablumsById %d: %v", id, err)
+	}
+	return alb, nil
+}
+
+// addAlbum adds the specified album to the database
+// returning the album ID of the new entry
+func addAlbum(alb Album) (int64, error) {
+	var albID int64
+	row := db.QueryRow(
+		"INSERT INTO album (title, artist, price) VALUES ($1, $2, $3) RETURNING id",
+		alb.Title,
+		alb.Artist,
+		alb.Price,
+	)
+	if err := row.Scan(&albID); err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+
+	return albID, nil
 }
